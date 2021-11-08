7ed42b5ae3f183ba9c63bbbf64159b364f5b2cd8
Andrea <andrea.calderonz@udea.edu.co>
2021-11-08 15:34:29 -0500

    complete

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'new-module'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('new-module');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import clients from '../files/clients.json';
+
+interface Client {
+  personId: string;
+  name: string;
+  lastName: string;
+  currentRole: string;
+  country: string;
+  industry: string;
+  numberOfRecommendations: string;
+  numberOfConnections: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'new-module';
+  occurrences: any = {};
+  avOccurrences: any = {};
+  allClients = clients;
+  countrys: string[] = clients.map((client: Client) => client.country);
+  totalClients: any = this.countrys.length;
+
+  averageCountry() {
+    this.occurrences = this.countrys.reduce((acc: any, curr: any) => {
+      //Ternary operator
+      return acc[curr] ? ++acc[curr] : (acc[curr] = 1), acc;
+    }, {});
+
+    this.avOccurrences = Object.keys(this.occurrences).forEach(key => {         
+      this.occurrences[key] = this.occurrences[key] / this.totalClients
+      return this.occurrences[key];
+    }, {});  
+    return this.occurrences;
+  }
 }
